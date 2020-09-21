8dbbdb248e36e9fd08fa69663c8e6a5413c8ad6a
Miguel Costa <maiquedi@gmail.com>
2020-09-22 00:49:03 +0100

    Updates

---
+++ precache-manifest.a5f65eeae1573b0613631f276c68c15e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4181333057083004da2450c3d8e801de",
+    "url": "/da-epoca-portugal-client/index.html"
+  },
+  {
+    "revision": "8f47e06436bdbfb2a7ee",
+    "url": "/da-epoca-portugal-client/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52e6c6e4e4f0886fede5",
+    "url": "/da-epoca-portugal-client/static/js/2.8612aac5.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/da-epoca-portugal-client/static/js/2.8612aac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f47e06436bdbfb2a7ee",
+    "url": "/da-epoca-portugal-client/static/js/main.ad1968db.chunk.js"
+  },
+  {
+    "revision": "a7b15497f1d4407046fd",
+    "url": "/da-epoca-portugal-client/static/js/runtime-main.c6d4d67e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1894ce6df345ded405ed979e1261c83d.js"
+  "/da-epoca-portugal-client/precache-manifest.a5f65eeae1573b0613631f276c68c15e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/da-epoca-portugal-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
